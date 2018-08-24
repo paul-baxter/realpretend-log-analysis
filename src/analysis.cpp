7b6210cf6adb6361a900a70d742b1f37ecdc64a8
paul-baxter <pebaxter@gmail.com>
2018-08-24 17:22:48 +0100

    handling new log files from SS2018

--- src/analysis.cpp
+++ src/analysis.cpp
@@ -35,6 +35,7 @@ int main (int argc, char **argv)
     //results containers
     int score;
     string participantID;
+    string condition;
     vector<string> timingAll;        //reaction times of all stories
     vector<string> timingTrain;      //reaction times of first four stories
     vector<string> timingTest;       //reaction times of rest of stories
@@ -42,6 +43,9 @@ int main (int argc, char **argv)
     vector<float> correctTrain;       //correctness of first four stories
     vector<float> correctTest;       //correctness of rest of stories
     vector<string> storyCorrectness(16);    //for each story, is it correct or not?
+    vector<string> storyOrderCorrect;   //in the order of presentation, story correctness
+    string startTime;
+    string duration;
 
     //arg parsing
     if (argc == 2)
@@ -62,8 +66,10 @@ int main (int argc, char **argv)
         //cout << fileName << endl;
     	inFile.open(fileName.c_str(), ios::in);
         getline(inFile, readIn);
-        cout << "PARTICIPANT: " + readIn << endl;
-        participantID = readIn;
+        vector<string> id = Splitter(readIn, ',');
+        cout << "PARTICIPANT: " + id[0] << endl;
+        participantID = id[0];
+        condition = id[1];
     }
     catch (std::ios_base::failure &fail)
     {
@@ -73,6 +79,14 @@ int main (int argc, char **argv)
 
     //throw away the second line, as this is data header
     getline(inFile, readIn);
+
+    //third line contains the start time stamp, want this...
+    getline(inFile, readIn);
+    vector<string> startTimeLine = Splitter(readIn, ',');
+    if((startTimeLine[0] == "event") && (startTimeLine.size() == 8))
+    {
+        startTime = startTimeLine[7];
+    }
 
     //the first four stories treated separately
     for (int a = 0; a < 4; a++)
@@ -107,28 +121,44 @@ int main (int argc, char **argv)
         //read until end: each line is now data
     	//std::cout << readIn << std::endl;
         vector<string> s = Splitter(readIn, ',');
-        //update score
-        if (s[1] == "1")
+
+        if ((s[0] == "data") && (s.size() == 6))
         {
-            score += 1; //increment score in line with Corriveau 2009...
+            //update score
+            if (s[1] == "1")
+            {
+                //increment score in line with Corriveau 2009...
+                score += 1;
+                //
+                // children received a 1 whenever they categorized a figure as real and
+                // a 0 whenever they categorized a figure as pretend. Thus, correct responding
+                // resulted in high scores ... for historical figures and low scores ...
+                // for fictional figures.
+            }
+            //timings
+            timingAll.push_back(s[3]);
+            timingTest.push_back(s[3]);
+            //correctness
+            if (answers[(int)StoF(s[4])] == s[1])
+            {
+                //if this is the case, then story has been classified correctly
+                correctTest.push_back(1.0f);
+                correctAll.push_back(1.0f);
+                storyCorrectness[(int)StoF(s[4])] = "1";
+            }
+            else
+            {
+                //story classified incorrectly
+                correctTest.push_back(0.0f);
+                correctAll.push_back(0.0f);
+                storyCorrectness[(int)StoF(s[4])] = "0";
+            }
         }
-        //timings
-        timingAll.push_back(s[3]);
-        timingTest.push_back(s[3]);
-        //correctness
-        if (answers[(int)StoF(s[4])] == s[1])
+
+        //final line provides the duration of the interaction with the robot
+        if ((s[0] == "event") && (s.size() == 10))
         {
-            //if this is the case, then story has been classified correctly
-            correctTest.push_back(1.0f);
-            correctAll.push_back(1.0f);
-            storyCorrectness[(int)StoF(s[4])] = "1";
-        }
-        else
-        {
-            //story classified incorrectly
-            correctTest.push_back(0.0f);
-            correctAll.push_back(0.0f);
-            storyCorrectness[(int)StoF(s[4])] = "0";
+            duration = s[9];
         }
     }
 
@@ -169,7 +199,7 @@ int main (int argc, char **argv)
             //if the file doesn't exist, then open, and write header
             cout << "Opening new file for results..." << endl;
             resultsFile.open(resultsFileName.c_str(), ios::out);
-            resultsFile << "ID,TrainCorrect,TestCorrect,AllCorrect,TimeMeanTrain,TimeSDTrain,TimeMeanTest,TimeSDTest,TimeMeanAll,TimeSDAll, testScore, story 1, story 2, story 3, story 4, story 5, story 6, story 7, story 8, story 9, story 10, story 11, story 12, story 13, story 14, story 15, story 16" << endl;
+            resultsFile << "ID,condition,TrainCorrect,TestCorrect,AllCorrect,TimeMeanTrain,TimeSDTrain,TimeMeanTest,TimeSDTest,TimeMeanAll,TimeSDAll,duration,testScore,story 1,story 2,story 3,story 4,story 5,story 6,story 7,story 8,story 9,story 10,story 11,story 12,story 13,story 14,story 15,story 16" << endl;
             resultsFile.flush();
         }
         else
@@ -186,12 +216,14 @@ int main (int argc, char **argv)
     resultsFile.exceptions(std::ios::goodbit);	//disable exception throwing
 
     resultsFile << participantID << ",";
+    resultsFile << condition << ",";
     resultsFile << correctnessTrain << ",";
     resultsFile << correctnessTest << ",";
     resultsFile << correctnessAll << ",";
     resultsFile << Mean(timingTrain) << "," << SD(timingTrain) << ",";
     resultsFile << Mean(timingTest) << "," << SD(timingTest) << ",";
     resultsFile << Mean(timingAll) << "," << SD(timingAll) << ",";
+    resultsFile << duration << ",",
     resultsFile << score << ",";
     //now the individual stories
     for (int a = 0; a < (int)correctAll.size(); a++)
